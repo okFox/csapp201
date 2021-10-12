95c1721367d33f8373af78b0ac76595737177661
okFox <smayfield@gmail.com>
2021-10-12 15:25:21 -0700

    bitwise.c done

--- assignment_01/bitwise.c
+++ assignment_01/bitwise.c
@@ -1,5 +1,7 @@
 /* assignment 1 - part B
- *  
+ *  In the program there is a function that takes an unsigned integer num (note this is always
+positive or zero) and an int position pos (0..31) and returns the value of the (pos)th bit as an integer (this will be either 1, 0 or -1 for error). Check for pos being between 0 and 31 and if it's not return a -1.
+--Need to use at least one bitwise logic operator and at least one shift operator in your function somewhere.
  */
 
 #include <stdio.h>
@@ -8,10 +10,9 @@
 
 int bit_value(unsigned number, int pos)
 {
-    // add your code here
-    return 0;
+    int bit = (number >> pos) & 1;
+    return bit;
 }
-
 
 int main()
 {
